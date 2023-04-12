fbaff9d6b2074a5ab1465f773d54ad7958741ad0
Obinna Obed <oobed@usc.edu>
2023-04-12 05:53:58 +0000

    hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <numeric>
 #endif
 
 // add or remove necessary headers as you please
@@ -18,27 +19,60 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
+#include "schedwork.h"
 
-// Add prototypes for any helper functions here
+#include <algorithm>
 
+// Recursive helper function for schedule()
 
-// Add your implementation of schedule() and other helper functions here
-
-bool schedule(
+bool Helper(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
-    DailySchedule& sched
-)
-{
-    if(avail.size() == 0U){
-        return false;
+    DailySchedule& sched,
+    size_t day,
+    map<Worker_T, size_t>& workerAvail
+){
+    if (day == sched.size()) {
+        return true;
     }
-    sched.clear();
-    // Add your code below
-
-
-
-
+    for(Worker_T i = 0; i < avail[day].size(); i++) {
+      if(avail[day][i] && workerAvail[i] < maxShifts) {
+        sched[day].push_back(i);
+        workerAvail[i]++;
+        if(sched[day].size() == dailyNeed) {
+          if(Helper(avail, dailyNeed, maxShifts, sched, day + 1, workerAvail)) {
+            return true;
+                }
+            } 
+            else{
+              if(Helper(avail, dailyNeed, maxShifts, sched, day, workerAvail)) {
+                return true;
+                }
+            }
+            sched[day].pop_back();
+            workerAvail[i]--;
+        }
+    }
+    return false;
 }
 
+
+bool schedule(const AvailabilityMatrix& avail,
+              const size_t dailyNeed,
+              const size_t maxShifts,
+              DailySchedule& sched) 
+{
+    if (avail.empty()) {
+        return false;
+    }
+    
+    sched.clear();
+    sched.resize(avail.size(), std::vector<Worker_T>());
+    map<Worker_T, size_t> workerAvail;
+    for (Worker_T i = 0; i < avail[0].size(); i++) {
+        workerAvail[i] = 0;
+    }
+    return Helper(avail, dailyNeed, maxShifts, sched, 0, workerAvail);
+   
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,52 +5,98 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <queue>
+#include <string>
 #endif
 
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
+  void helper(queue<char>floating_letters, set<string>& word_valid, size_t idx,string in, set<string>& dict);
 
-// Add prototypes of helper functions here
-bool isWordValid(const string& word, const string& in, const string& floating);
 
-// Definition of primary wordle function
-std::set<std::string> wordle(
+  void helper(queue<char> floating_letters, set<string>& word_valid, size_t idx, string in, const set<string>& dict) {
+    int ndashes = 0;
+    set<char> already_filled;
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    if(idx == in.size()) {
+        if (dict.count(in)) {
+            word_valid.insert(in);
+        }
+        return;
+    }
+    for(char c : in) {
+      ndashes += (c == '-');
+    }
+    if(in[idx] == '-') {
+      if(ndashes < floating_letters.size()) {
+            return;
+        }       
+      for(int i = 0; i < floating_letters.size(); i++) {
+        char character = floating_letters.front();
+        in[idx] = character;
+        floating_letters.pop();
+        if(already_filled.count(character) == 0) {
+          helper(floating_letters, word_valid, idx+1, in, dict);
+          already_filled.insert(character);
+            } 
+          floating_letters.push(character);
+          in[idx] = '-';
+        }
+        
+        if(ndashes == floating_letters.size()) {
+          return;
+        }
+        for(char c : alphabet) {
+          in[idx] = c;            
+          if(already_filled.count(c) == 0) {
+            helper(floating_letters, word_valid, idx+1, in, dict);
+            }
+            
+          in[idx] = '-';
+        }
+    } else{
+        helper(floating_letters, word_valid, idx+1, in, dict);
+    }
+}
+
+    
+  set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
-    set<string> result;
-    string allChars = "abcdefghijklmnopqrstuvwxyz";
-
-    for (auto it = dict.begin(); it != dict.end(); it++) {
-        string word = *it;
-        if (isWordValid(word, in, floating)) {
-            result.insert(word);
-        }
+    const std::set<std::string>& dict){
+      queue<char>floating_letters;
+      for(int i = 0; i<floating.size();i++){
+        floating_letters.push(floating[i]);
+      }
+      set<string>words_valid;
+      helper(floating_letters,words_valid, 0, in, dict);
+      return words_valid;
     }
+    
+    
+    
+    
+    
+//     //calculate the number of blanks left for each recursive call
+//     //check if the idx is a dash
+//     ///if its already a letter, skip it
+//     ///if its a dash, 
+//     ////iterate through all floating letters
+//     ////pop from the queue and put it in the location of the idx
+//     ////call the recursive function, with the new word
+//     ////put a letter back in the queue
+//     ////reset the location back to a dash
+    
 
-    return result;
+//     //iterate through the alphabet
+//     //set current idx to the current letter 
+//     //do the recursive call with the word
+//     //reset back to a blanks
 
+//     //at the top, make a new set of characters
+//     //if a letter is already used, skip over the recursive call
+//     ///if your'e inserting a floating letter, add it to the set of floating letters
 
-}
-
-bool isWordValid(const string& word, const string& in, const string& floating) {
-    if (word.length() != in.length() + floating.length()) {
-        return false;
-    }
-    for (int i = 0; i < in.length(); i++) {
-        if (in[i] != word[i]) {
-            return false;
-        }
-    }
-    for (int i = 0; i < floating.length(); i++) {
-        if (word.find(floating[i]) == string::npos) {
-            return false;
-        }
-    }
-    return true;
-}
-// Define any helper functions here
